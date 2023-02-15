a90ecc5ddb47dce232331f9aaf678bd2b8f95191
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-02-15 15:36:01 -0300

    aula_165

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
-
+// useContext
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
 
 const About = () => {
+  const {contextValue} = useContext(SomeContext);
+
   return (
     <div>
-     About
+     <h2>About</h2>
+     <p>Valor do context: {contextValue}</p>
     </div>
   );
 }
